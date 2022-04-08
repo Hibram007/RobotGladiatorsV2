2c3b0037addd6dba3a051b1ebe1f226328d58c6a
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2022-04-08 14:48:03 -0700

    file structure and js functions for prompts and variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,24 @@
-window.alert("This is an alert! JavaScript is running!");
 
+// setting a variable to capture input of prompt to user
+var playerName = window.prompt("What is your name?");
+
+//function called to check data is correctly stored in var
+// will give an alert with the user input to the prompt
+console.log(playerName);
+console.log("This is a string message");
+// this does math
+console.log(10 + 10);
+// this combines a string and a variable
+console.log("Our robot's name is" + playerName);
+console.log("Hello"  + playerName + "I am AresCom, leader of the the arena");
+
+// this creates a function named "fight"
+
+// we are defining what the function "fight" does when invoked
+ 
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+// Calling the function - do the thing its predetermined to do
+//fight();
